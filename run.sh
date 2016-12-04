db9dc2f0f7f4069bc6b50feaa84b635d170e3f8e
Arseniy Shestakov <me@arseniyshestakov.com>
2016-12-04 08:48:49 +0300

    Add full path to vcmiserver
    For some reason /usr/games is not in user's PATH

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    until vcmiserver; do
+    until /usr/games/vcmiserver; do
         echo "Server crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
